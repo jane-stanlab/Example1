20270acc4dc77c4702481956227f1674a3774eab
jane-stanlab <zhaoz9@mail.uc.edu>
2021-01-31 16:58:56 -0500

    This is my example with notes

--- Example1.R
+++ Example1.R
@@ -1,15 +1,25 @@
-#This is my exapm;e
+# This is my example with notes 
 
 
 file <- "TCGA_breast_cancer_LumA_vs_Basal_PAM50.tsv" #variable type: string
 first10 <- c('NAT1','BIRC5','BAG1','BCL2','BLVRA','CCNB1','CCNE1','CDC6','CDC20','CDH3')
 nfold <- 3
-
-header <- scan(file, nlines = 1, sep="\t", what = character()) #In the csv, rows are genes, and columns are samples
-data <- read.table(file, skip = 2, header = FALSE, sep = "\t", quote = "", check.names=FALSE)#skip first 2 lines,
+header <- scan(file, nlines = 1, sep="\t", what = character()) #use the function of "scan" to read the file, and read only one line
+#print (header)
+data <- read.table(file, skip = 2, header = FALSE, sep = "\t", quote = "", check.names=FALSE)
 names(data) <- header
+#print (data)
+#dim(data) #dimension of the data--->rows,columns
+#data$sample_id #show column labeled “sample_id” – gene names
+#data$sample_id %in% c("BCL2","NAT1") #apply operater %in% to filter 2 genes, c()works for make the genes a list [c:concatenate]
+#as.character(data$sample_id) #a better way to see genes
+#data$sample_id == "BCL2" #filer the sample id to be BCL2
+#data[data$sample_id=="BCL2",] #All samples from gene "BCL2". Don't forget the comma
+#data[,c(6)] #data of column 6
+#data[c(6)] #data of colume 6, easier to watch
 
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t", what = character()) #read the line of "class"
+#data[,header2=='Luminal A'] #filer on columns
 
 LumA <- data[data$sample_id %in% first10,header2=='Luminal A']
 Basal <- data[data$sample_id %in% first10,header2=='Basal-like']
